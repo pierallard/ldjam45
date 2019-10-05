38726eddb6a6276dbcba2b1a53e2eae910037df7
toxinu <toxinu@gmail.com>
2019-10-05 17:58:20 +0200

    Double ration

--- src/game/Prison.ts
+++ src/game/Prison.ts
@@ -7,10 +7,13 @@ export default class Prison extends Phaser.State {
   public create(game: Phaser.Game) {
     this.map = game.add.tilemap('prison', 16, 16);
     this.map.addTilesetImage("dungeon_sheet", "tiles");
+    this.map.enableDebug = true;
 
     const floor = this.map.createLayer("floor");
     const walls = this.map.createLayer("walls");
     const items = this.map.createLayer("items");
+
+    console.log(this.map)
 
     // layer.resizeWorld();
   }
@@ -20,6 +23,14 @@ export default class Prison extends Phaser.State {
   }
 
   public canGoTo(position: Point) {
+    const floorTile = this.map.getTile(position.x, position.y, "floor");
+    if (floorTile) {
+      console.log(position.x, position.y, floorTile)
+    }
+    const itemTile = this.map.getTile(position.x, position.y, "items");
+    if (itemTile) {
+      console.log(position.x, position.y, itemTile)
+    }
     return true;
   }
 }
