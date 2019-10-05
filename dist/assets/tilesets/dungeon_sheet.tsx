b8577141f6dd8a3380a25fa9a41fe9ad29a38c6f
Adrien Pétremann <hello@grena.fr>
2019-10-05 17:02:36 +0200

    exporte taaz

---
+++ dist/assets/tilesets/dungeon_sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="dungeon_sheet" tilewidth="16" tileheight="16" tilecount="240" columns="24">
+ <image source="dungeon_sheet.png" width="384" height="160"/>
+</tileset>
