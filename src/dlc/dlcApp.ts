69d1fa5b36f032e6518118b263fedad8deb2c0c8
Adrien Pétremann <hello@grena.fr>
2019-10-06 21:12:07 +0200

    installed

--- src/dlc/dlcApp.ts
+++ src/dlc/dlcApp.ts
@@ -39,7 +39,7 @@ const createDLCItem = (dlc: DLC, index: number, onBuy: (dlc: DLC) => void) => {
 
   if (dlc.isAcheted) {
     const price = getClass('dlcPrice', dlcItem);
-    price.innerText = `Already acheted`;
+    price.innerText = `DLC Installed`;
     const buy = getClass('dlcBuyButton', dlcItem);
     buy.remove();
   } else {
