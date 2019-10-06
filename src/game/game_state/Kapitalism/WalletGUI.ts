ed8a68b47cddd8285a0d2253d71448a15950c17f
Nicolas Dupont <nicolas@akeneo.com>
2019-10-06 21:51:04 +0200

    add dlc price

--- src/game/game_state/Kapitalism/WalletGUI.ts
+++ src/game/game_state/Kapitalism/WalletGUI.ts
@@ -1,19 +1,29 @@
 import Wallet from "./Wallet";
+import {DLC} from "../../DLCs";
 
 export default class WalletGUI {
 
     private wallet: Wallet;
     private text: Phaser.Text;
+    private dlc: DLC;
+    private dlcText: Phaser.Text;
 
     constructor(wallet: Wallet) {
         this.wallet = wallet;
     }
 
     public create(game: Phaser.Game) {
-        this.text = game.add.text(1000, 10, this.wallet.total() + " USD", { font: "35px Gloria Hallelujah", fill: "#333333" });
+        this.text = game.add.text(850, 10, 'Wallet: ' + this.wallet.total() + " USD", { font: "35px Gloria Hallelujah", fill: "#333333" });
+        this.dlcText = game.add.text(850, 50, 'DLC price: ' + 0 + " USD", { font: "35px Gloria Hallelujah", fill: "#333333" });
     }
 
     public update(game: Phaser.Game) {
-        this.text.text = this.wallet.total() + " USD";
+        this.text.text = 'Wallet: ' + this.wallet.total() + " USD";
+        this.dlcText.text = 'DLC price: ' + this.dlc.price + " USD";
+
+    }
+
+    public setDLC(dlc: DLC) {
+        this.dlc = dlc;
     }
 }
