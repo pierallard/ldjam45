14a0c89865f7764059d342b81dd98dd9e1bc1220
pierallard <pierallard@gmail.com>
2019-10-06 10:59:19 +0200

    Refacto

--- src/game/game_state/DungeonLevel1.ts
+++ src/game/game_state/DungeonLevel1.ts
@@ -1,55 +1,35 @@
-import {DungeonPlayer} from "../DungeonPlayer";
 import {Door} from "../Door";
 import Point from "../Point";
-import {MessageBox} from "../MessageBox";
 import Prison from "../Prison";
-import MenuDLC from '../MenuDLC';
-import {Pie} from "../Pie";
 import {DLCItem} from "../DLCList";
 import PlayerRoom from "./PlayerRoom";
 import TilemapsProperties from "../TilemapsProperties";
-import {Cursor} from "./Cursor";
-import {SCALE, TILE_SIZE} from "../../app";
+import {AbstractDungeonLevel} from "./AbstractDungeonLevel";
 
-export default class DungeonLevel1 extends Phaser.State {
-  private player: DungeonPlayer;
-  private messageBox: MessageBox;
-  private tilemap: Prison;
-  private tilemapProperties: TilemapsProperties;
-  private menuDLC: MenuDLC;
+export default class DungeonLevel1 extends AbstractDungeonLevel {
   private showDoorMessage: boolean;
-  private pie: Pie;
   private showBeginningMessage: boolean;
-  private cursor: Cursor;
   private paypal: Phaser.Image;
   private paypalAlreadyMontred: boolean;
 
-  constructor(sprite: Phaser.Sprite) {
+  constructor() {
     super();
     this.tilemapProperties = new TilemapsProperties();
-    this.player = new DungeonPlayer(new Point(1, 2));
-    this.messageBox = null;
     this.tilemap = new Prison(this, this.tilemapProperties, this.player);
     this.showDoorMessage = true;
     this.showBeginningMessage = true;
-    this.pie = null;
-    this.menuDLC = new MenuDLC(true);
     this.paypalAlreadyMontred = false;
   }
 
+  public getStartPosition(): Point {
+    return new Point(1, 2);
+  }
+
   public create(game: Phaser.Game) {
-    this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
-    this.game.scale.setUserScale(SCALE, SCALE);
+    super.create(game);
 
-    this.game.renderer.renderSession.roundPixels = true;
-    Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
-
-    this.tilemap.create(game);
-
-    this.player.create(game, this.tilemap);
     if (this.showBeginningMessage) {
       this.showBeginningMessage = false;
-
       this.addMessageBox(game, 'je suis enfermay ! Je dois sortir!', () => {
         this.player.stopPlayer();
         game.time.events.add(0.5 * Phaser.Timer.SECOND, () => {
@@ -60,60 +40,34 @@ export default class DungeonLevel1 extends Phaser.State {
       });
     }
 
-    this.menuDLC.create(game, (dlcItem: DLCItem) => {
-      if (!this.paypalAlreadyMontred) {
-        this.paypalAlreadyMontred = true;
-        this.paypal.visible = true;
-        this.cursor.enablez(false);
-        game.add.tween(this.cursor).to({
-          x: 156,
-          y: 101
-        }, 2 * Phaser.Timer.SECOND, Phaser.Easing.Default, true);
-        game.time.events.add(2 * Phaser.Timer.SECOND, () => {
-          game.state.start('PlayerRoom');
-          this.cursor.enablez(true);
+    this.paypal = game.add.image(0, 0, 'paypal');
+    this.paypal.visible = false;
+  }
 
-          const playerRoom = game.state.states['PlayerRoom'];
-          (<PlayerRoom>playerRoom).setdlcItem(dlcItem);
-          dlcItem.achete();
-        });
-      } else {
+  public getDlcCallback(game: Phaser.Game, dlcItem: DLCItem) {
+    if (!this.paypalAlreadyMontred) {
+      this.paypalAlreadyMontred = true;
+      this.paypal.visible = true;
+      this.cursor.enablez(false);
+      game.add.tween(this.cursor).to({
+        x: 156,
+        y: 101
+      }, 2 * Phaser.Timer.SECOND, Phaser.Easing.Default, true);
+      game.time.events.add(2 * Phaser.Timer.SECOND, () => {
         game.state.start('PlayerRoom');
         this.cursor.enablez(true);
 
         const playerRoom = game.state.states['PlayerRoom'];
         (<PlayerRoom>playerRoom).setdlcItem(dlcItem);
         dlcItem.achete();
-      }
-    });
-
-    this.paypal = game.add.image(0, 0, 'paypal');
-    this.paypal.visible = false;
-
-    this.cursor = new Cursor(game);
-    this.game.add.existing(this.cursor);
-  }
-
-  public update(game: Phaser.Game) {
-    this.cursor.update2(game);
-    if (null !== this.messageBox) {
-      if (this.messageBox.update(game)) {
-        this.messageBox = null;
-      }
-      return;
-    }
-    if (null !== this.pie) {
-      if (this.pie.update(game)) {
-        this.pie = null;
-      }
-      return;
-    }
-    this.player.update(game);
-    if (this.showDoorMessage && this.tilemap.getActivable(this.player.getPosition()) instanceof Door) {
-      this.player.stopPlayer();
-      this.showDoorMessage = false;
-      this.addMessageBox(game, 'Appuyay sur Entray pour crochtay la porte', () => {
       });
+    } else {
+      game.state.start('PlayerRoom');
+      this.cursor.enablez(true);
+
+      const playerRoom = game.state.states['PlayerRoom'];
+      (<PlayerRoom>playerRoom).setdlcItem(dlcItem);
+      dlcItem.achete();
     }
   }
 
@@ -122,21 +76,15 @@ export default class DungeonLevel1 extends Phaser.State {
       this.game.debug.body(this.player.sprite);
   }
 
-  public addMessageBox(game: Phaser.Game, message: string, callback) {
-    this.messageBox = new MessageBox(message, callback);
-    this.messageBox.create(game);
-  }
-
-  public addPie(game: Phaser.Game, position: Point, duration: number, callback: any) {
-    this.pie = new Pie(position, duration, callback);
-    this.pie.create(game);
-  }
-
-  public displayDLCButton() {
-    this.menuDLC.displayButton();
-  }
-
-  public hasAchetedDlc(name): boolean {
-    return this.menuDLC.dlcIsAcheted(name);
+  public update(game: Phaser.Game) {
+    if (super.update(game)) {
+      if (this.showDoorMessage && this.tilemap.getActivable(this.player.getPosition()) instanceof Door) {
+        this.player.stopPlayer();
+        this.showDoorMessage = false;
+        this.addMessageBox(game, 'Appuyay sur Entray pour crochtay la porte', () => {
+        });
+      }
+    }
+    return true;
   }
 }
