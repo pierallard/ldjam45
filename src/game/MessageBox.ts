8e2e20d86a967ba953d62593a322cecd470fcf24
pierallard <pierallard@gmail.com>
2019-10-06 16:03:32 +0200

    message box

--- src/game/MessageBox.ts
+++ src/game/MessageBox.ts
@@ -1,65 +1,74 @@
-import {MOVE_TIME} from "./DungeonPlayer";
 import BitmapText = Phaser.BitmapText;
-import Point from "./Point";
 
 export class MessageBox {
   private message;
   private shouldRenderText;
   private bitmapText: BitmapText;
   private spacebar: Phaser.Key;
-  private graphics: Phaser.Graphics;
+  private graphics: Phaser.Image;
   private callback: any;
+  private spaceBarDown: boolean;
 
   constructor(str: string, callback: any) {
     this.message = str;
     this.callback = callback;
+    this.spaceBarDown = false;
   }
 
   create(game: Phaser.Game) {
-    this.graphics = game.add.graphics(0, 0);
-    this.graphics.beginFill(0x444444);
-    this.graphics.lineStyle(1, 0xffffff);
-    this.graphics.drawRect(20.5, 150.5, 260, 20);
-
-    this.bitmapText = game.add.bitmapText(22, 152, 'Carrier Command', '', 5);
+    this.graphics = game.add.image(20, 150, 'messagebox');
+    this.bitmapText = game.add.bitmapText(28, 158, 'Carrier Command', '', 5);
     this.shouldRenderText = true;
     this.spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   }
 
   update(game: Phaser.Game): boolean {
+    if (this.shouldRenderText) {
+      this.shouldRenderText = false;
+      game.time.events.add(0.1 * Phaser.Timer.SECOND, () => {
+        const length = this.bitmapText.text.length;
+        this.bitmapText.text = this.message.substr(0, length + 1);
+        this.shouldRenderText = true;
+      });
+    }
+
     if (this.spacebar.isDown) {
-      if (this.bitmapText.text === this.message) {
-        if (this.shouldRenderText) {
-          this.bitmapText.destroy(true);
-          this.graphics.destroy(true);
-
-          this.callback();
-
-          return true;
-        }
-      } else {
-        this.bitmapText.text = this.message;
-        this.shouldRenderText = false;
-        game.time.events.add(0.5 * Phaser.Timer.SECOND, () => {
-          this.shouldRenderText = true;
-        });
-
+      if (this.spaceBarDown) {
         return false;
       }
+      this.spaceBarDown = true;
+
+      return this.actSpaceBar();
+    } else if (this.spacebar.isUp) {
+      this.spaceBarDown = false;
     }
-    if (this.bitmapText.text === this.message) {
-      return false;
-    }
-    if (!this.shouldRenderText) {
-      return false;
-    }
-    this.shouldRenderText = false;
-    game.time.events.add(0.1 * Phaser.Timer.SECOND, () => {
-      const length = this.bitmapText.text.length;
-      this.bitmapText.text = this.message.substr(0, length + 1);
-      this.shouldRenderText = true;
-    });
 
     return false;
+  }
+
+  private actSpaceBar(): boolean {
+    if (!this.textIsFullyDisplayed()) {
+      this.displayFullText();
+      return false;
+    } else {
+      this.removeBox();
+
+      this.callback();
+
+      return true;
+    }
+  }
+
+  private textIsFullyDisplayed() {
+    return this.bitmapText.text === this.message;
+  }
+
+  private displayFullText() {
+    this.bitmapText.text = this.message;
+  }
+
+  private removeBox() {
+    this.bitmapText.destroy(true);
+    this.graphics.destroy(true);
   }
 }
