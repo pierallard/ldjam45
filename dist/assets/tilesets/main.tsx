e9ce0ea7b6f14e4cd115ee1a336cc9da002f1c0a
Adrien Pétremann <hello@grena.fr>
2019-10-07 21:15:05 +0200

    azdazd

---
+++ dist/assets/tilesets/main.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="main" tilewidth="16" tileheight="16" tilecount="72" columns="24">
+ <image source="interior.png" width="384" height="48"/>
+ <tile id="9">
+  <properties>
+   <property name="name" value="freedoor"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="name" value="secretary"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="name" value="digicode"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="name" value="water"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="name" value="coca"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="name" value="door"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="name" value="computer"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="name" value="iench"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="name" value="vigil"/>
+  </properties>
+ </tile>
+</tileset>
